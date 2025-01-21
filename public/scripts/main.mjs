576a49581a0d7d990130dafbe022d889bfeeba44
Adedero <118962395+Adedero@users.noreply.github.com>
2025-01-22 00:13:47 +0100

    Complete books and events route

--- public/scripts/main.mjs
+++ public/scripts/main.mjs
@@ -13,3 +13,34 @@ window.addEventListener('scroll', () => {
     header.style.zIndex = 10
   }
 })
+
+const nav = document.querySelector('#nav')
+const menuBtn = document.querySelector('#menu-btn')
+const closeBtn = document.querySelector('#close-btn')
+
+if (menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    nav?.classList.toggle('active')
+  })
+}
+
+if (closeBtn) {
+  closeBtn.addEventListener('click', () => {
+    nav?.classList.remove('active')
+  })
+}
+
+const navLinks = document.querySelectorAll('[data-nav]')
+if (navLinks) {
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      nav?.classList.remove('active')
+    })
+    const href = link.getAttribute('href')
+    if (window.location.pathname === href) {
+      link.classList.add('active')
+    } else {
+      link.classList.remove('active')
+    }
+  })
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,41 @@ const { Router } = require('express')
 
 const router = Router()
 
+const socials = require('../data/social-media-links')
+
 const routes = () => {
   router.get('/', (req, res) => {
-    const socials = require('../data/social-media-links')
-
     res.render('index', {
+      socials
+    })
+  })
+
+  router.get('/about', (req, res) => {
+    res.render('about', {
+      socials
+    })
+  })
+
+  router.get('/books', (req, res) => {
+    res.render('books', {
+      socials
+    })
+  })
+
+  router.get('/books/:slug', (req, res) => {
+    res.render('book', {
+      socials
+    })
+  })
+
+  router.get('/events', (req, res) => {
+    res.render('events', {
+      socials
+    })
+  })
+
+  router.get('/contact', (req, res) => {
+    res.render('contact', {
       socials
     })
   })
